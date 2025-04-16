7c0f3ef987affc1c7dc682bd5174876088a6ee1e
addispic <addispik.com>
2025-04-16 18:04:41 +0300

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,7 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-
-// pages
-import Home from "./pages/Home";
-
 const App = () => {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-      </Routes>
+      <h3>Failure...</h3>
     </div>
   );
 };
