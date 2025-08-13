9cf763dcfdb210f5ffd03e0f16be1c77a571eab1
addispic <addispik.com>
2025-08-13 16:28:57 +0300

    dev branch just modified to the latest version

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div className="w-screen h-screen flex items-center justify-center bg-[#02030a]">
-      <h3 className="text-3xl text-neutral-500 uppercase">Wait...</h3>
+      <h3 className="text-xs text-neutral-500 lowercase">Wait...</h3>
     </div>
   );
 };
