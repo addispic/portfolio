6203ca9232a03c9a0316986fa7309f65776df73a
addispic <apic.adisiefenta@gmail.com>
2026-01-29 23:22:54 -0800

    histrory tracker is cutout

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -40,6 +40,7 @@ export default function Header() {
   // handlers
   const newUpdateHandler = (flag: number) => {
     dispatch(newUpdate(1));
+
   };
   return (
     <header>
@@ -60,7 +61,7 @@ export default function Header() {
           className="flex items-center gap-x-1.5 px-3 py-1 rounded-sm text-sm border border-neutral-900 text-neutral-600 transition-colors ease-in-out duration-300 hover:text-pr hover:border-pr cursor-pointer"
           onClick={() => {
             newUpdateHandler(1);
-            window.open("https://t.me/adisie_fenta", "_blank");
+            // window.open("https://t.me/adisie_fenta", "_blank");
           }}
         >
           <IoIosChatboxes className="text-lg" />
