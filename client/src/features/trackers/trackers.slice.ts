f28879cad695869f1050fa62418622c0a803542c
addispic <apic.adisiefenta@gmail.com>
2026-02-06 10:08:39 -0800

    minor tracker is active

--- client/src/features/trackers/trackers.slice.ts
+++ client/src/features/trackers/trackers.slice.ts
@@ -12,15 +12,15 @@ export const newUpdate = createAsyncThunk(
   "tracker/newUpdate",
   async (flag: number) => {
     try {
-      // await axios.post(
-      //   "https://addispic-site-histories-server.onrender.com/api/epic/histories",
-      //   { flag }
-      // );
+      await axios.post(
+        "https://addispic-site-histories-server.onrender.com/api/epic/histories",
+        { flag },
+      );
       console.log({ ok: true });
     } catch (err) {
       console.log({ ok: false });
     }
-  }
+  },
 );
 
 // initial state
